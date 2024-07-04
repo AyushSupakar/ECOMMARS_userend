81ef0e0baaa8f4b1ef9827bff03e98e0872d35cd
Ayush Anshuman Supakar <supakarayush@gmail.com>
2024-07-04 19:33:51 +0530

    fixed the search bar

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -77,7 +77,7 @@ export default function MyNavBar(props) {
 
   <div className="flex items-center">
     <form className="flex flex-grow" onSubmit={searchhandle}><div className="flex flex-grow">
-      <input type="search" value={sterm} onChange={(ev)=>setsterm(ev?.target?.value)} className="flex-grow m-0 text-end border-1 rounded-l-xl" placeholder="search..." /></div>
+      <input type="search" value={sterm} onChange={(ev)=>setsterm(ev?.target?.value)} className="flex-grow m-0 text-start border-1 px-2 rounded-xl" placeholder="search..." /></div>
    <div className="flex"><button type="submit" className="sbtn-primary"><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg></button></div></form>
 
     
@@ -141,7 +141,7 @@ export default function MyNavBar(props) {
 
   <div className="flex items-center">
     <form className="flex flex-grow" onSubmit={searchhandle}><div className="flex flex-grow">
-      <input type="search" value={sterm} onChange={(ev)=>setsterm(ev?.target?.value)} className="flex-grow m-0 text-end border-1 rounded-l-xl" placeholder="search..." /></div>
+      <input type="search" value={sterm} onChange={(ev)=>setsterm(ev?.target?.value)} className="flex-grow m-0 text-start border-1 px-2 rounded-xl" placeholder="search..." /></div>
    <div className="flex"><button type="submit" className="sbtn-primary"><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg></button></div></form>
 
     
